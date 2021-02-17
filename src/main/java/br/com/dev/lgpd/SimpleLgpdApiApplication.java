1884112a8138d04b0a3fe77a8c78fad3b03c32e7
Guilherme Neres <guilhermeneresbsb@gmail.com>
2021-02-17 14:47:57 -0300

    [LGPD] init poc project

---
+++ src/main/java/br/com/dev/lgpd/SimpleLgpdApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.dev.lgpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleLgpdApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleLgpdApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/dev/lgpd/SimpleLgpdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dev.lgpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleLgpdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
